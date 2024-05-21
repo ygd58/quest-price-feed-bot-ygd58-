8e077e0bc7a560606cc16977114122a42f91e33f
buray <inalmef@gmail.com>
2024-05-21 22:16:49 +0300

    Create 0rbit-Price-Feed.lua

---
+++ 0rbit-Price-Feed.lua
@@ -0,0 +1,57 @@
+-- PID   Oe0fT2LNnyPmpVNrDr6-pecm-8mzWLO-V_8kFmNxAB8
+
+local json = require("json")
+
+_ORBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
+
+function handleError(msg, errorMessage)
+    ao.send({
+        Target = msg.From,
+        Tags = {
+            Action = "Error",
+            ["Message-Id"] = msg.Id,
+            Error = errorMessage
+        }
+    })
+end
+
+Handlers.add("https://github.com/ygd58",
+    Handlers.utils.hasMatchingTag("Action", "Sponsored-Get-Request"),
+    function(msg)
+        local token = msg.Tags.Token
+        if not token then
+            handleError(msg, "Token not provided")
+            return
+        end
+        
+        local url = "https://api.coingecko.com/api/v3/simple/price?ids=" .. token .. "&vs_currencies=usd"
+        ao.send({
+            Target = _ORBIT,
+            Action = "Get-Real-Data",
+            Url = url
+        })
+        print("Pricefetch request sent for " .. token)
+    end
+)
+
+Handlers.add("ReceiveData",
+    Handlers.utils.hasMatchingTag("Action", "Receive-Response"),
+    function(msg)
+        print("Received data: " .. msg.Data)
+        local res = json.decode(msg.Data)
+        local token = msg.Tags.Token
+        if res[token] and res[token].usd then
+            ao.send({
+                Target = msg.From,
+                Tags = {
+                    Action = "Price-Response",
+                    ["Message-Id"] = msg.Id,
+                    Price = res[token].usd
+                }
+            })
+            print("Price of " .. token .. " is " .. res[token].usd)
+        else
+            handleError(msg, "Failed to fetch price")
+        end
+    end
+)
